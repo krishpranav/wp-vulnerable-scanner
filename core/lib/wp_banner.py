9db89e9362b5a563673feff1ee43023eec37f1e2
krishpranav <krisna.pranav@gmail.com>
2021-03-01 19:32:42 +0530

    done changes

---
+++ core/lib/wp_banner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/python
+# this is a file made for banner for the tool
+
+import wp_colors 
+import wp_info
+
+def Banner():
+	print wp_colors.WPColors().red(1)+r"WP-SCANNER"+wp_colors.WPColors().end()   
+	print ""	
