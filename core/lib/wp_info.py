bdcf180e1963d954fe9274ae41eca02e7846870a
krishpranav <krisna.pranav@gmail.com>
2021-03-01 19:41:44 +0530

    done changes

---
+++ core/lib/wp_info.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+class WPInfo:
+	#wpscan info
+	def name(self):
+		return "wp-scanner"
+
+	def version(self):
+		return "Version 0.2.0"
+
+	def author(self):
+		return "KrishPranav"
+	
+	def repo(self):
+		return "https://github.com/krishpranav/wp-vulnerable-scanner"
